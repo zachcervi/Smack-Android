f18de500fb6b4d741dd072eb054be3b604138742
Zach Cervi <zachcervi@gmail.com>
2019-04-13 15:14:21 -0600

    Login in user upon creating an account

--- app/src/main/java/Utilities/Constants.kt
+++ app/src/main/java/Utilities/Constants.kt
@@ -2,3 +2,4 @@ package Utilities
 
 const val BASE_URL = "https://chatappsmack18.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
--- app/src/main/java/com/zachcervi/smack_android/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/zachcervi/smack_android/Controller/CreateUserActivity.kt
@@ -37,10 +37,20 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View){
-        AuthService.registerUser(this, "zach@cervimobilesolutions.com", "123456"){
-                complete ->
-            if(complete){
+        val email = createEmailTxt.text.toString()
+        val password = createPasswordTxt.text.toString()
 
+
+        AuthService.registerUser(this, email, password){
+                registerSuccess ->
+            if(registerSuccess){
+                AuthService.loginUser(this, email, password){
+                    loginSuccess ->
+                    if(loginSuccess){
+                        println(AuthService.authToken)
+                        println(AuthService.userEmail)
+                    }
+                }
             }
         }
         
