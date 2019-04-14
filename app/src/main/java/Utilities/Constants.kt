27bb37dfa95bd0e00d7cf98d7b24ed89dee54114
Zach Cervi <zachcervi@gmail.com>
2019-04-13 18:41:56 -0600

    clean up and added spinner

--- app/src/main/java/Utilities/Constants.kt
+++ app/src/main/java/Utilities/Constants.kt
@@ -4,3 +4,6 @@ const val BASE_URL = "https://chatappsmack18.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
+
+//Broadcast Constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
--- app/src/main/java/com/zachcervi/smack_android/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/zachcervi/smack_android/Controller/CreateUserActivity.kt
@@ -2,10 +2,14 @@ package com.zachcervi.smack_android.Controller
 
 import Services.AuthService
 import Services.UserDataService
+import Utilities.BROADCAST_USER_DATA_CHANGE
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import com.zachcervi.smack_android.R
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
@@ -18,6 +22,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View){
@@ -38,30 +43,48 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View){
+        enableSpinner(true)
        val userName = createUserNameTxt.text.toString()
         val email = createEmailTxt.text.toString()
         val password = createPasswordTxt.text.toString()
 
-
-        AuthService.registerUser(this, email, password){
-                registerSuccess ->
-            if(registerSuccess){
-                AuthService.loginUser(this, email, password){
-                    loginSuccess ->
-                    if(loginSuccess){
-                        AuthService.createUser(this, userName, email, userAvatar, avatarColor) {createSuccess ->
-                            if(createSuccess){
-                                println(UserDataService.name)
-                                println(UserDataService.avatarColor)
-                                println(UserDataService.id)
-                                finish()
+        if(userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.registerUser(this, email, password){
+                    registerSuccess ->
+                if(registerSuccess){
+                    AuthService.loginUser(this, email, password){
+                            loginSuccess ->
+                        if(loginSuccess){
+                            AuthService.createUser(this, userName, email, userAvatar, avatarColor) {createSuccess ->
+                                if(createSuccess){
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    enableSpinner(false)
+                                    finish()
+                                }else {
+                                    errorToast()
+                                }
                             }
+                        }else {
+                            errorToast()
                         }
                     }
+                } else {
+                    errorToast()
                 }
             }
         }
+        else {
+            Toast.makeText(this, "Make sure user name, email and password are not empty.", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
+        }
+
+
         
+    }
+    private fun errorToast(){
+        Toast.makeText(this, "Something went wrong try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
     }
 
     fun generateColorClicked(view: View){
@@ -77,5 +100,17 @@ class CreateUserActivity : AppCompatActivity() {
 
         avatarColor = "[$savedR, $savedG, $savedB, 1]" //API format
 
+    }
+
+    fun enableSpinner(enable: Boolean){
+        if(enable){
+            createSpinner.visibility = View.VISIBLE
+        }else {
+            createSpinner.visibility = View.INVISIBLE
+        }
+
+        createCreateUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
     }
 }
